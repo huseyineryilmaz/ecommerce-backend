78af41fb27ebeff32e84399dc951ac64ee808992
huseyineryilmaz <huseyin.eryilmaz1996@gmail.com>
2022-02-07 00:55:30 +0300

    redux installed

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {productListReducer} from './reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers({
+    productList: productListReducer
+});
+
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+    initialState,
+    composeEnchancer(applyMiddleware(thunk))
+);
+
+export default store;
