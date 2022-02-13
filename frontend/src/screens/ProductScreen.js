def0864f0fc53c5e68d0bcc493917974948fd22e
huseyineryilmaz <huseyin.eryilmaz1996@gmail.com>
2022-02-13 11:12:13 +0300

    refactor

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import {detailsProduct} from '../actions/productActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
@@ -8,7 +8,8 @@ import Rating from '../components/Rating';
 
 export default function ProductScreen(props) {
     const dispatch = useDispatch();
-    const productId = props.match.params.id;
+    const params = useParams();
+    const {id: productId} = params;
     const productDetails = useSelector((state) => state.productDetails);
     const {loading, error, product} = productDetails;
 
