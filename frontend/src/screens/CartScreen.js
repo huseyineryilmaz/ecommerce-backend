4712189b8297a1175ce3ca70243666e862134c00
huseyineryilmaz <huseyin.eryilmaz1996@gmail.com>
2022-02-17 18:06:47 +0300

    add to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import {useLocation, useParams} from 'react-router-dom';
+import {addToCart} from '../actions/cartActions';
 
 export default function CartScreen() {
     const params = useParams();
     const productId = params.id;
     const {search} =useLocation();
     const qtyInUrl = new URLSearchParams(search).get('qty');
-    const qty = qtyInUrl?Number(qtyInUrl):1;
+    const qty = qtyInUrl ? Number(qtyInUrl) : 1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
     return (
         <div>
             <h1>Cart Screen</h1>
