707e2eceb79a493fea20fb43a6edefc09882f806
Huseyin Eryilmaz <dev.huseyineryilmaz@gmail.com>
2022-07-24 23:37:12 +0300

    products with mongodb

--- backend/data.js
+++ backend/data.js
@@ -17,7 +17,6 @@ const data = {
     ],
     products: [
         {
-            _id: '1',
             name: 'Lemon Slim Jean',
             category: 'Jean',
             image: '/images/p1.jpg',
@@ -29,8 +28,7 @@ const data = {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
         },
         {
-            _id: '2',
-            name: 'Lemon Slim Jean',
+            name: 'Lemon Medium Jean',
             category: 'Jean',
             image: '/images/p1.jpg',
             price: 354,
@@ -41,8 +39,7 @@ const data = {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
         },
         {
-            _id: '3',
-            name: 'Lemon Slim Jean',
+            name: 'Lemon Large Jean',
             category: 'Jean',
             image: '/images/p1.jpg',
             price: 586,
@@ -53,8 +50,7 @@ const data = {
             description: 'Lorem ipsum dolor sit amet.'
         },
         {
-            _id: '4',
-            name: 'Lemon Slim Jean',
+            name: 'Factory Slim Jean',
             category: 'Jean',
             image: '/images/p1.jpg',
             price: 652,
@@ -65,8 +61,7 @@ const data = {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
         },
         {
-            _id: '5',
-            name: 'Lemon Slim Jean',
+            name: 'Factory Medium Jean',
             category: 'Jean',
             image: '/images/p1.jpg',
             price: 145,
@@ -77,8 +72,7 @@ const data = {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula dui eget tortor fringilla tristique. Proin aliquet libero ac tellus hendrerit ultricies.'
         },
         {
-            _id: '6',
-            name: 'Lemon Slim Jean',
+            name: 'Factory Large Jean',
             category: 'Jean',
             image: '/images/p1.jpg',
             price: 185,
